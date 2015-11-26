aa6a9f0376f538ff25701bbdeff2684359279e87
David Hsu <dvdhsu@gmail.com>
2015-11-26 03:06:24 +0000

    Add seeds, and check for ok on insertions.

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -12,10 +12,21 @@
 
 alias Fiberboard.Repo
 alias Fiberboard.City
+alias Fiberboard.AttractionCategory
+alias Fiberboard.Attraction
 
 london_center = %Geo.Point{coordinates: {-0.102216, 51.51786}, srid: 4326}
 london_params = %{center: london_center, name: "London"}
 
 changeset = City.changeset(%City{}, london_params)
 
-Repo.insert(changeset)
+{:ok, london} = Repo.insert(changeset)
+
+parks_params = %{name: "Parks"}
+changeset = AttractionCategory.changeset(%AttractionCategory{}, parks_params)
+{:ok, parks} = Repo.insert(changeset)
+
+hyde_park_coords = %Geo.Point{coordinates: {-0.177704, 51.507776}, srid: 4326}
+hyde_park_params = %{name: "Hyde Park", category_attraction_id: parks.id, city_id: london.id, location: hyde_park_coords}
+changeset = Attraction.changeset(%Attraction{}, hyde_park_params)
+hyde_park = Repo.insert(changeset)
